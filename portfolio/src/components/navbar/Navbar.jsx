c4b78f1ab5462cf9c62b720c6f199dc36504637f
taiwo <taiwooyindamola515@gmail.com>
2021-11-24 00:32:05 +0100

    Components anf folder setup

---
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './navbar.css'
+function Navbar() {
+    return (
+        <div>
+            <h1>Set Up Complete</h1>
+        </div>
+    )
+}
+
+export default Navbar
